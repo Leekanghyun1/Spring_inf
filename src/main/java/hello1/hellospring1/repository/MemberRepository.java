8b6b4d8df1e0322f953d7b0e7d6ba817713de561
leekanghyun <tjd576@naver.com>
2022-04-24 22:09:05 +0900

    0424

---
+++ src/main/java/hello1/hellospring1/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello1.hellospring1.repository;
+
+import hello1.hellospring1.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/main/java/hello1/hellospring1/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package hello1.hellospring1.repository;
+
+import hello1.hellospring1.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository  implements MemberRepository{
+    
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); 
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();  //돌면서 name이 같으면 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); //멤버들이 반환
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
