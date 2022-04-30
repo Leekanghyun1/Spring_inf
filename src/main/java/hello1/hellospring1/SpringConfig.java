abb5ac28e6aa548c64c6d58741f07c1ecd0246f9
leekanghyun <tjd576@naver.com>
2022-04-30 23:59:45 +0900

    0430

--- src/main/java/hello1/hellospring1/SpringConfig.java
+++ src/main/java/hello1/hellospring1/SpringConfig.java
@@ -1,19 +1,31 @@
 package hello1.hellospring1;
 
 
+import hello1.hellospring1.repository.JdbcMemberRepository;
 import hello1.hellospring1.repository.MemberRepository;
 import hello1.hellospring1.repository.MemoryMemberRepository;
 import hello1.hellospring1.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
-    public class SpringConfig { @Bean
+    public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
         @Bean
         public MemberRepository memberRepository() {
-            return new MemoryMemberRepository();
+            // return new MemoryMemberRepository();
+            return new JdbcMemberRepository(dataSource);
         }
 }
