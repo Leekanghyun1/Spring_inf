f8f6c2f5d7f687b77200af1865674db26e6beda2
leekanghyun <tjd576@naver.com>
2022-05-02 09:50:28 +0900

    0501

--- src/main/java/hello1/hellospring1/service/MemberService.java
+++ src/main/java/hello1/hellospring1/service/MemberService.java
@@ -32,7 +32,7 @@ public class MemberService {
     private  void validateDuplicateMember(Member member){
         memberRepository.findByName(member.getName())
         .ifPresent(me -> {
-            throw  new IllegalStateException("이미 존재하는 회원입니다. ");
+            throw  new IllegalStateException("이미 존재하는 회원입니다.");
         });
 
     }
